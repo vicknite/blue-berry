b646fd459b4e7d72df2e4c207e266eb1c7f1e9e7
vicky fazlurrahman <vicky@go-jek.com>
2016-09-20 09:16:22 +0700

    initial commit

---
+++ app/src/main/java/com/frupia/perawallet/perawallet/login/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.frupia.perawallet.perawallet.login;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.frupia.perawallet.perawallet.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
